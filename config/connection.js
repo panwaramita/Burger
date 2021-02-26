9b4fcce90c71a4efb84a1441ed1e2669d5036560
Amita <panwar.amita@gmail.com>
2021-02-26 01:26:58 -0500

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,24 @@
 const mysql=require('mysql');
-connection = mysql.createConnection({
-    host: "pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "d5pytxxh1jy7j36t",
-    password: "rxlqmmawnbl1j2ed",
-    database: "mkvvtsb6l4ys5cgx"
+let connection;
+if(process.env.JAWSDB_URL)
+{
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+connection=mysql.createConnection({
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    password:"090981",
+    database:"burgers_db"
 });
-connection.connect();
+}
+connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 module.exports=connection;
