b16ed8de465a9de7fd9c26a502a572aa622c81f6
Amita <panwar.amita@gmail.com>
2021-02-26 01:21:40 -0500

    added the readme file

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,10 @@
 const mysql=require('mysql');
-let connection;
-if(process.env.JAWSDB_URL)
-{
-    connection=mysql.createConnection(process.env.JAWSDB_URL);
-}
-else
-{
-connection=mysql.createConnection({
-    host:"localhost",
-    port:"3306",
-    user:"root",
-    password:"090981",
-    database:"burgers_db"
+connection = mysql.createConnection({
+    host: "pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "d5pytxxh1jy7j36t",
+    password: "rxlqmmawnbl1j2ed",
+    database: "mkvvtsb6l4ys5cgx"
 });
-}
 connection.connect();
 module.exports=connection;
