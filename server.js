919d76bcb66fac03b0ee4e5d591b098ed6c3cf50
Amita <panwar.amita@gmail.com>
2021-02-26 00:41:42 -0500

    updated the server file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var path=require('path');
-var PORT = process.env.PORT || 3000;
+const express = require("express");
+const path=require('path');
+const PORT = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 //app.use(express.static("public"));
@@ -12,13 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
