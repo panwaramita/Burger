457864dd34403111c00ad76addda9f379598933f
Amita <panwar.amita@gmail.com>
2021-02-26 01:52:09 -0500

    updated server file

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const path=require('path');
+const connection = require("./config/connection");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-//app.use(express.static("public"));
-app.use(express.static("public"));
-// Parse application body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// //app.use(express.static("public"));
+// app.use(express.static("public"));
+// // Parse application body
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// Set Handlebars.
-const exphbs = require("express-handlebars");
+// // Set Handlebars.
+// const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller.js");
+// // Import routes and give the server access to them.
+// const routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+// app.use(routes);
+app.get("/",function(req,res){
+res.send("hello");
+})
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
